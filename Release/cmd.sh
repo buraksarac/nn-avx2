9a8734b61f2d2c2b45f9b88e83aa6f6d567081cf
buraksarac <burak@linux.com>
2020-04-04 00:16:36 +0200

    no random for test

--- Release/cmd.sh
+++ Release/cmd.sh
@@ -1,1 +1,1 @@
-xperf stat -e task-clock,cycles,instructions,cache-references,cache-misses,branches,branch-misses,faults,minor-faults,cs,migrations -r 3 nice taskset 0x01 ./nn-avx2 -x ../xShuffled.dat -y ../yShuffled.dat -r 5000 -c 400 -n 10 -t 1 -h 256 -i 320 -l 0.99 -j 32 -test 15 -ps 64 -cpus 32 
+xperf stat -e task-clock,cycles,instructions,cache-references,cache-misses,branches,branch-misses,faults,minor-faults,cs,migrations -r 3 nice taskset 0x01 ./nn-avx2 -x ../xShuffled.dat -y ../yShuffled.dat -r 5000 -c 400 -n 10 -t 1 -h 256 -i 320 -l 0.99 -j 32 -test 15 -ps 64 -cpus 32 -rand 0 
